dcb05de54fa7910c93b32e89fb285f9cc9d39e23
danehoov <DaneHoov22@gmail.com>
2024-04-25 15:46:02 -0600

    inheritance solved

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,28 @@
 const Person = require('./person');
 
-// Your code here 
+class Student extends Person {
+
+  constructor (firstName, lastName, major, GPA) {
+    super(firstName, lastName)
+    this.major = major
+    this.GPA = GPA
+  }
+
+  static compareGPA (student1, student2) {
+    let smartestStudent;
+
+    if(student1.GPA > student2.GPA) {
+      smartestStudent = student1
+    }
+    else if (student2.GPA > student1.GPA) {
+    smartestStudent = student2
+    }
+    else if (student1.GPA === student2.GPA) {
+      return "Both students have the same GPA"
+    }
+    return `${smartestStudent.firstName} ${smartestStudent.lastName} has the higher GPA.`
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
-// Your code here 
+class Teacher extends Person {
+
+  constructor (firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName)
+    this.subject = subject
+    this.yearsOfExperience = yearsOfExperience
+  }
+
+  static combinedYearsOfExperience(teachers) {
+      let experience = 0
+
+      for(let teacher of teachers) {
+        experience += teacher.yearsOfExperience
+      }
+      return experience
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
